91a519ec4cfdadaa996c55137422b6ac20d5c6bc
palalexfossan <palalex.fossan@gmail.com>
2026-05-03 09:42:33 +0200

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,42 @@
+export const screenerConfig = {
+  rangeThreshold: 0.30,
+  minYield: 8.0,
+  intervalMinutes: 30,
+  aiOnSignal: false,
+  defaultTickers: [
+    // Shipping
+    "GOGL.OL",    // Golden Ocean
+    "FRO.OL",     // Frontline
+    "MPCC.OL",    // MPC Container Ships
+    "HAFNI.OL",   // Hafnia
+    "BELCO.OL",   // Belships
+    "WULF.OL",    // Wallenius Wilhelmsen
+    "HAVI.OL",    // Havila Shipping
+    "2020.OL",    // 2020 Bulkers
+    "HUNTER.OL",  // Hunter Group
+    "KCC.OL",     // Klaveness Combination Carriers
+    "SHLF.OL",    // Shelf Drilling
+    // Energi & offshore
+    "AKRBP.OL",   // Aker BP
+    "SUBC.OL",    // Subsea 7
+    "PGS.OL",     // PGS
+    "TGS.OL",     // TGS
+    "BORR.OL",    // Borr Drilling
+    "SDRL.OL",    // Seadrill
+    "AGAS.OL",    // Avance Gas
+    "FLNG.OL",    // Flex LNG
+    // Finans
+    "DNB.OL",     // DNB Bank
+    "MOWI.OL",    // Mowi
+    "SRBANK.OL",  // SpareBank 1 SR-Bank
+    "SVEG.OL",    // SpareBank 1 SMN
+    "MING.OL",    // SpareBank 1 Nord-Norge
+    // Industri & annet
+    "NHY.OL",     // Norsk Hydro
+    "TEL.OL",     // Telenor
+    "NSKOG.OL",   // Norske Skog
+    "AKER.OL",    // Aker ASA
+    "OET.OL",     // Odfjell Technology
+    "OKEA.OL",    // OKEA
+  ],
+};
